738f9e13496ee0faffcb773bc0ce694c228f0cfc
Martorelli <luis.martorelli@bm-maritima.cl>
2024-02-07 16:35:21 -0300

    src/

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,9 +1,14 @@
-import { createContainer, InjectionMode } from 'awilix';
+import { asClass, createContainer, InjectionMode } from 'awilix';
+import AuthService from '../services/AuthService';
+import AuthController from '../controllers/AuthController';
 
 const container = createContainer({
   injectionMode: InjectionMode.CLASSIC,
 });
 
-container.register({});
+container.register({
+  authService: asClass(AuthService).singleton(),
+  authController: asClass(AuthController).singleton(),
+});
 
 export default container;
